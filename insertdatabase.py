492f3bfdb4674d5c6fbfa4962e86eeae6f6d2654
Lazar-source <56961536+Lazar-source@users.noreply.github.com>
2020-01-09 09:50:27 +0000

    Add files via upload

---
+++ insertdatabase.py
@@ -0,0 +1,20 @@
+import mysql.connector
+from datetime import datetime
+from sense_hat import SenseHat
+import time
+
+
+
+def insert(pitch,roll,yaw,x,y,z):
+    mydb=mysql.connector.connect(host="localhost", user="Raspberry", password="root", database="exampledb4")
+    mycursor=mydb.cursor()
+    now=datetime.now()
+    formatted_date=now.strftime('%Y-%m-%d %H:%M:%S')
+    sql="""INSERT INTO adatok (datum,pitch,roll,yaw,backwardforward,rightleft,updown)
+    VALUES(%s,%s,%s,%s,%s,%s,%s)"""
+    val=(formatted_date,pitch,roll,yaw,x,y,z)
+    mycursor.execute(sql,val)
+    mydb.commit()
+    time.sleep(0.2)
+    mycursor.close()
+    mydb.close()
